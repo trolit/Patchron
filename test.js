0b228c68cf049093385cd205d57503bfc8abf45a
pawel idzikowski <iddziku@gmail.com>
2022-01-19 18:12:40 +0100

    Create test.js

---
+++ test.js
@@ -0,0 +1,33 @@
+// SIMPLE "FLAVOUR" TESTS
+
+// breakline above return
+function test1() {
+    const w = 2;   
+    return w;
+}
+
+// unnecessary breakline
+function test2() {
+
+    return 2;
+}
+
+// use const instead of let & var
+function test3() {
+    let w = 55;
+    var e = 4;
+
+    return (w + e);
+}
+
+function test4() {
+    
+}
+
+// typo
+funetion test5() {
+    return 2;
+}
+
+
+
