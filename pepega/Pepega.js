70203315ead3d57ad85ee811b6bcc66133d516d1
pawel idzikowski <iddziku@gmail.com>
2022-01-21 23:42:12 +0100

    Add small desc to investigate static method in Pepega.js

--- pepega/Pepega.js
+++ pepega/Pepega.js
@@ -2,6 +2,7 @@ const FileLoader = require('./loaders/File');
 
 class Pepega {
     /**
+     * Investigates file against selected rules.
      * @param {object} file File from pull request.
      * @param file.name The name of the file.
      * @param file.content file content in base64 encoding.
