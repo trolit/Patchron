4f8ca8fb9044ac8b93113652ccd04202ceb1c5ad
pawel idzikowski <iddziku@gmail.com>
2022-01-24 21:14:03 +0100

    Update BasicDataBuilder.js

--- pepega/helpers/BasicDataBuilder.js
+++ pepega/helpers/BasicDataBuilder.js
@@ -1,27 +1,25 @@
-/** Class representing basic intel that will be used in all rules. */
+/** Class representing basic intel that is given to all rules. */
 class BasicDataBuilder {
     /**
-     * @param {object} repo information provided via Probot's repo()
-     * @param {string} repo.owner repository owner's name
-     * @param {string} repo.repo repository name
-     * @param {string} pull_number pull request Id
-     * @param {string} patch changes made to file
-     * @param {string} path relative path to the file
-     * @param {string} commit_id
+     * @param {object} file
+     * @param {string} file.owner repository owner's name
+     * @param {string} file.repo repository name
+     * @param {string} file.pull_number pull request Id
+     * @param {string} file.patch changes made to file
+     * @param {string} file.path relative path to the file
+     * @param {string} file.commit_id
      */
-    constructor(repo, pull_number, file) {
-        const { filename, contents_url, patch } = file;
+    constructor(file) {
+        const { filename, contents_url } = file;
 
         const commit_id = contents_url.split('ref=').pop();
 
         const path = filename;
 
         const content = {
-            ...repo,
-            pull_number,
+            ...file,
             commit_id,
             path,
-            patch,
         };
 
         return content;
