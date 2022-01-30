0a397ef85b7cc968b8531706ee2e595935e518e9
pawel idzikowski <iddziku@gmail.com>
2022-01-31 00:12:38 +0100

    Update getNearestHunkHeader.js

--- pepega/helpers/getNearestHunkHeader.js
+++ pepega/helpers/getNearestHunkHeader.js
@@ -1,7 +1,7 @@
 /**
- * returns nearest **hunk data**
- * @param {Array<string>} split_content array including code
- * @param {number} row_number index of current loop iteration.
+ * returns nearest **hunk**
+ * @param {Array<string>} splitContent array including code
+ * @param {number} row index of current loop iteration.
  *
  * @link https://www.edureka.co/community/7949/what-are-these-in-github
  *
@@ -14,17 +14,17 @@
  * │    2    │              '+'              │
  * └─────────┴───────────────────────────────┘
  * {
- * | hunk_position: 0,
- * | source_file: { start_line: '0', hunk_length: '0' },
- * | modified_file: { start_line: '1', hunk_length: '33' }
+ * | index: 0,
+ * | sourceFile: { line: '0', length: '0' },
+ * | modifiedFile: { line: '1', length: '33' }
  * }
  *
  * @returns {object}
  */
-module.exports = (split_content, row_number) => {
+module.exports = (splitContent, row) => {
     let result = null;
 
-    if (!Array.isArray(split_content) || !row_number) {
+    if (!Array.isArray(splitContent) || !row) {
         probotInstance.log.warn(
             `Invalid data passed to the function -> ${__filename}`
         );
@@ -32,28 +32,28 @@ module.exports = (split_content, row_number) => {
         return result;
     }
 
-    for (let i = row_number; i >= 0; i--) {
-        const row_content = split_content[i];
+    for (let i = row; i >= 0; i--) {
+        const rowContent = splitContent[i];
 
-        if (row_content.startsWith('@@')) {
-            const split_row_content = row_content.split(' ');
+        if (rowContent.startsWith('@@')) {
+            const splitRowContent = rowContent.split(' ');
 
-            if (split_row_content.length < 3) {
+            if (splitRowContent.length < 3) {
                 return result;
             }
 
-            const source_file_data = split_row_content[1].split(/[-,]/);
-            const modified_file_data = split_row_content[2].split(/[+,]/);
+            const sourceFile = splitRowContent[1].split(/[-,]/);
+            const modifiedFile = splitRowContent[2].split(/[+,]/);
 
             result = {
-                hunk_position: i,
-                source_file: {
-                    start_line: source_file_data[1],
-                    hunk_length: source_file_data[2],
+                index: i,
+                sourceFile: {
+                    line: sourceFile[1],
+                    length: sourceFile[2],
                 },
-                modified_file: {
-                    start_line: modified_file_data[1],
-                    hunk_length: modified_file_data[2],
+                modifiedFile: {
+                    line: modifiedFile[1],
+                    length: modifiedFile[2],
                 },
             };
 
