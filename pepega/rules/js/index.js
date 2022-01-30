5a828bd7c61d7870dea8e318e36fffa4237b934f
pawel idzikowski <iddziku@gmail.com>
2022-01-30 01:41:06 +0100

    Update index.js

--- pepega/rules/js/index.js
+++ pepega/rules/js/index.js
@@ -1,3 +1,17 @@
-class Js {}
+class Js {
+    static review(file, rules) {
+        let comments = [];
+
+        for (const rule in rules) {
+            let rule_comments = rules[rule].invoke(file);
+
+            if (rule_comments.length) {
+                comments.push(...rule_comments);
+            }
+        }
+
+        return comments;
+    }
+}
 
 module.exports = Js;
