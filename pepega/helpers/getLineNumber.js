e4de5fd5def7397090c6e92ca3de1f7af5d3f289
pawel idzikowski <iddziku@gmail.com>
2022-01-31 18:21:48 +0100

    Update getLineNumber.js

--- pepega/helpers/getLineNumber.js
+++ pepega/helpers/getLineNumber.js
@@ -1,7 +1,7 @@
 /**
  * returns line number of current row (single line comments only)
- * @param {string} start_line start_line of **source_file** or **modified_file** hunk header
- * @param {number} row_number
+ * @param {string} startLine startLine of **source_file** or **modified_file** hunk header
+ * @param {number} row
  *
  * @example
  * e.g. to get line number for row 2:
@@ -12,10 +12,10 @@
  * │    1    │     '+// SIMPLE TESTS'        │
  * │    2    │          '+// RR'             │
  * └─────────┴───────────────────────────────┘
- * => 15 (start_line) + 2 (row_number) - 1 = 16
+ * => 15 (startLine) + 2 (row) - 1 = 16
  *
  * @returns {number}
  */
-module.exports = (start_line, row_number) => {
-    return parseInt(start_line) + row_number - 1;
+module.exports = (startLine, row) => {
+    return parseInt(startLine) + row - 1;
 };
