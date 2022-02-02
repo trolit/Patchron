a68c9d9a9b483bff361aee16cba3c0f636a5bf59
pawel idzikowski <iddziku@gmail.com>
2022-02-02 16:26:58 +0100

    Update getFiles.js

--- pepega/requests/getFiles.js
+++ pepega/requests/getFiles.js
@@ -5,20 +5,22 @@
  * @param {string} payload.owner repository owner's name
  * @param {string} payload.repo repository name
  * @param {string} payload.pull_number pull request Id
- * @returns {Array<object>} array of files.
+ * @returns {Promise<Array<object>>}
  */
 module.exports = async (context, payload) => {
     let files = [];
 
     try {
         files = await context.octokit.pulls.listFiles(payload);
+
+        return Promise.resolve(files.data);
     } catch (error) {
         probotInstance.log.error(
             `Failed to fetch files from PR:${payload.pull_number} of ${payload.repo} -> ${__filename}`
         );
 
         probotInstance.log.error(error);
-    }
 
-    return files;
+        return Promise.reject();
+    }
 };
