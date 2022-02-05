7b1d99dea28c859881d64f512727cd9f0ac8ccff
pawel idzikowski <iddziku@gmail.com>
2022-02-05 11:54:50 +0100

    Update getFiles.js

--- pepega/requests/getFiles.js
+++ pepega/requests/getFiles.js
@@ -1,14 +1,15 @@
+const { settings } = require('../config');
+
 /**
- * fetches files from specified pull request
+ * fetches files from pull request. Unpaginated response includes a maximum of 3000 files
+ * so **it's not recommended** to overload GitHub API by multiple small requests in 99% cases.
  * @param {object} context WebhookEvent instance.
  * @param {object} payload repository data.
  * @param {string} payload.owner repository owner's name
  * @param {string} payload.repo repository name
  * @param {string} payload.pull_number pull request Id
- *
- * @description
- * Unpaginated response include a maximum of 3000 files. The paginated response returns
- * 30 files per page by default. **Octokit paginate** allows to get accumulated results.
+ * @param {string} [payload.per_page = 30] results per page (max 100)
+ * @param {string} [payload.page] page number of the results to fetch.
  *
  * @link
  * https://octokit.github.io/rest.js/v18#pulls-list-files
@@ -19,11 +20,17 @@ module.exports = async (context, payload) => {
     let files = [];
 
     try {
-        files = await context.octokit.paginate(
-            context.octokit.pulls.listFiles,
-            payload,
-            (response) => response.data
-        );
+        if (settings.isGetFilesRequestPaginated) {
+            files = await context.octokit.paginate(
+                context.octokit.pulls.listFiles,
+                payload,
+                (response) => response.data
+            );
+        } else {
+            const { data } = await context.octokit.pulls.listFiles(payload);
+
+            files = data;
+        }
 
         return Promise.resolve(files);
     } catch (error) {
