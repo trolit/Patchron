96b5e6ae88f764d4ba9e42635c1e0fa9db0582dc
pawel idzikowski <iddziku@gmail.com>
2022-02-06 17:51:25 +0100

    Create addSenderAsAssignee.js

---
+++ pepega/pull-request/addSenderAsAssignee.js
@@ -0,0 +1,21 @@
+const addAssignees = require('../requests/addAssignees');
+
+/**
+ * adds pull request sender as assignee
+ * @param {WebhookEvent<EventPayloads.WebhookPayloadPullRequest>} context WebhookEvent instance.
+ * @param {object} repo
+ * @param {string} repo.owner repository owner's name
+ * @param {string} repo.repo repository name
+ * @param {number} repo.pull_number pull request Id
+ * @param {object} payload
+ * @param {string} payload.sender login of user who created pull request
+ */
+module.exports = async (context, repo, payload) => {
+    const { login: pullRequestOwner } = payload.sender;
+
+    try {
+        await addAssignees(context, repo, [pullRequestOwner]);
+    } catch (error) {
+        probotInstance.log.error(error);
+    }
+};
