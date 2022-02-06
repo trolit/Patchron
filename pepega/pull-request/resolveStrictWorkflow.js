b775e3bb01988103bba53e9eed96b9fb31f2d31c
pawel idzikowski <iddziku@gmail.com>
2022-02-06 18:49:27 +0100

    Create resolveStrictWorkflow.js

---
+++ pepega/pull-request/resolveStrictWorkflow.js
@@ -0,0 +1,29 @@
+const addComment = require('../requests/addComment');
+
+/**
+ * validates pull requested branch against strict workflow rule and posts comment (if needed).
+ * @param {WebhookEvent<EventPayloads.WebhookPayloadPullRequest>} context WebhookEvent instance.
+ * @param {object} payload hooked PR data
+ * @param {object} rules config rules object
+ * @param {object} strictWorkflow strict workflow config
+ * @returns {boolean} flag if review should be continued or not
+ */
+module.exports = async (context, payload, rules, strictWorkflow) => {
+    const comment = rules.pull['strictWorkflow'].invoke(payload);
+
+    let isReviewAborted = false;
+
+    if (comment.reason === 'prefix') {
+        isReviewAborted = strictWorkflow.abortReviewOnInvalidBranchPrefix;
+    } else if (comment.reason === 'flow') {
+        isReviewAborted = strictWorkflow.abortReviewOnInvalidFlow;
+    }
+
+    try {
+        await addComment(context, comment.body);
+    } catch (error) {
+        probotInstance.log.error(error);
+    }
+
+    return isReviewAborted;
+};
