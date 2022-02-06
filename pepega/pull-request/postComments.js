7ef30850da632fc874cce575fb223004ce51045f
pawel idzikowski <iddziku@gmail.com>
2022-02-06 18:27:25 +0100

    Update postComments.js

--- pepega/pull-request/postComments.js
+++ pepega/pull-request/postComments.js
@@ -3,7 +3,7 @@ const addMultiLineReviewComment = require('../requests/addMultiLineReviewComment
 const addSingleLineReviewComment = require('../requests/addSingleLineReviewComment');
 
 /**
- * **POST** review comments
+ * **POST** review comments to GitHub
  * @param {WebhookEvent<EventPayloads.WebhookPayloadPullRequest>} context WebhookEvent instance.
  * @param {Array<object>} reviewComments
  * @param {number} delayBetweenCommentRequestsInSeconds
