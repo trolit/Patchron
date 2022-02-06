af55b1abae1e40fa31b84f129e0a97b8fec23934
pawel idzikowski <iddziku@gmail.com>
2022-02-06 12:52:11 +0100

    Update index.js

--- pepega/config/index.js
+++ pepega/config/index.js
@@ -1,3 +1,4 @@
+const strictWorkflow = require('../rules/pull/StrictWorkflow');
 const noUnmarkedComments = require('../rules/common/NoUnmarkedComments');
 
 require('dotenv').config({
@@ -5,44 +6,74 @@ require('dotenv').config({
 });
 
 module.exports = {
+    env: {},
     settings: {
         isGetFilesRequestPaginated: false,
         delayBetweenCommentRequestsInSeconds: 3,
         isOwnerAssigningEnabled: true,
         isReviewSummaryEnabled: true,
+        strictWorkflow: {
+            enabled: true,
+            abortReviewOnInvalidBranchPrefix: false,
+            abortReviewOnInvalidFlow: true,
+        },
     },
-    env: {},
     rules: {
-        js: [
-            {
-                instance: new noUnmarkedComments({
-                    prefixes: [
-                        {
-                            value: 'TODO:',
-                            meaning: 'needs to be implemented',
-                        },
-                        {
-                            value: '*:',
-                            meaning: 'important note',
-                        },
-                        {
-                            value: '!:',
-                            meaning: 'to be removed',
-                        },
-                        {
-                            value: '?:',
-                            meaning: 'suggestion',
-                        },
-                        {
-                            value: 'TMP:',
-                            meaning: 'temporary solution',
-                        },
-                    ],
-                    isAppliedToSingleLineComments: true,
-                    isAppliedToMultiLineComments: true,
-                    isAppliedToInlineComments: true,
-                }),
-            },
-        ],
+        pull: {
+            strictWorkflow: new strictWorkflow({
+                workflow: [
+                    {
+                        base: 'develop',
+                        head: 'release',
+                    },
+                    {
+                        base: 'develop',
+                        head: 'feature',
+                    },
+                    {
+                        base: 'master',
+                        head: 'develop',
+                    },
+                    {
+                        base: 'master',
+                        head: 'hotfix',
+                    },
+                ],
+            }),
+        },
+        files: {
+            common: [],
+            js: [
+                {
+                    instance: new noUnmarkedComments({
+                        prefixes: [
+                            {
+                                value: 'TODO:',
+                                meaning: 'needs to be implemented',
+                            },
+                            {
+                                value: '*:',
+                                meaning: 'important note',
+                            },
+                            {
+                                value: '!:',
+                                meaning: 'to be removed',
+                            },
+                            {
+                                value: '?:',
+                                meaning: 'suggestion',
+                            },
+                            {
+                                value: 'TMP:',
+                                meaning: 'temporary solution',
+                            },
+                        ],
+                        isAppliedToSingleLineComments: true,
+                        isAppliedToMultiLineComments: true,
+                        isAppliedToInlineComments: true,
+                    }),
+                },
+            ],
+        },
     },
 };
