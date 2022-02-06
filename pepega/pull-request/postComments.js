510e2459df6da019dcbbbd942bf8a9c0b78fb922
pawel idzikowski <iddziku@gmail.com>
2022-02-06 18:22:39 +0100

    Create postComments.js

---
+++ pepega/pull-request/postComments.js
@@ -0,0 +1,39 @@
+const timer = require('../helpers/loopTimer');
+const addMultiLineReviewComment = require('../requests/addMultiLineReviewComment');
+const addSingleLineReviewComment = require('../requests/addSingleLineReviewComment');
+
+/**
+ * POST review comments to pull request
+ * @param {WebhookEvent<EventPayloads.WebhookPayloadPullRequest>} context WebhookEvent instance.
+ * @param {Array<object>} reviewComments
+ * @param {number} delayBetweenCommentRequestsInSeconds
+ */
+module.exports = async (
+    context,
+    reviewComments,
+    delayBetweenCommentRequestsInSeconds
+) => {
+    for (let i = 0; i < reviewComments.length; i++) {
+        const reviewComment = reviewComments[i];
+
+        if (reviewComment.start_line) {
+            try {
+                await addMultiLineReviewComment(context, {
+                    ...reviewComment,
+                });
+            } catch (error) {
+                probotInstance.log.error(error);
+            }
+        } else if (reviewComment.line) {
+            try {
+                await addSingleLineReviewComment(context, {
+                    ...reviewComment,
+                });
+            } catch (error) {
+                probotInstance.log.error(error);
+            }
+        }
+
+        await timer(delayBetweenCommentRequestsInSeconds * 1000);
+    }
+};
