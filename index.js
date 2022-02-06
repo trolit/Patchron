16ab086a88d444c1295e5fa3af41aca8340eec1d
pawel idzikowski <iddziku@gmail.com>
2022-02-06 19:54:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // **************************************************
-// *: Pepega.js
-// *: made with Probot framework
-// *: source: https://github.com/trolit/Pepega
+// *: Pepega.js (2022)
+// *: made with Probot
+// *: my heart: https://github.com/trolit/Pepega
 // **************************************************
 
 /**
@@ -26,16 +26,15 @@
  *
  */
 
-const dedent = require('dedent-js');
-const Pepega = require('./pepega/Pepega');
-const timer = require('./pepega/helpers/loopTimer');
 const getFiles = require('./pepega/requests/getFiles');
 const { rules, settings } = require('./pepega/config');
-const addComment = require('./pepega/requests/addComment');
 const printBotName = require('./pepega/helpers/printBotName');
-const addAssignees = require('./pepega/requests/addAssignees');
-const addMultiLineReviewComment = require('./pepega/requests/addMultiLineReviewComment');
-const addSingleLineReviewComment = require('./pepega/requests/addSingleLineReviewComment');
+const postSummary = require('./pepega/pull-request/postSummary');
+const reviewPullRequest = require('./pepega/pull-request/review');
+const postComments = require('./pepega/pull-request/postComments');
+const initializeData = require('./pepega/pull-request/initialize');
+const resolveStrictWorkflow = require('./pepega/pull-request/resolveStrictWorkflow');
+const addPullSenderAsAssignee = require('./pepega/pull-request/addSenderAsAssignee');
 
 /**
  * This is the main entrypoint of Pepega Probot app
@@ -44,20 +43,25 @@ const addSingleLineReviewComment = require('./pepega/requests/addSingleLineRevie
 module.exports = (app) => {
     printBotName();
 
-    const { isOwnerAssigningEnabled, isReviewSummaryEnabled, strictWorkflow } =
-        settings;
+    const {
+        isOwnerAssigningEnabled,
+        isReviewSummaryEnabled,
+        strictWorkflow,
+        delayBetweenCommentRequestsInSeconds,
+    } = settings;
 
     global.probotInstance = app;
 
     app.on(
         ['pull_request.opened', 'pull_request.synchronize'],
         async (context) => {
-            const { payload, repo } = setup(context);
+            const { payload, repo } = initializeData(context);
 
             if (strictWorkflow.enabled) {
                 const isReviewAborted = resolveStrictWorkflow(
                     context,
                     payload,
+                    rules,
                     strictWorkflow
                 );
 
@@ -67,7 +71,7 @@ module.exports = (app) => {
             }
 
             if (isOwnerAssigningEnabled) {
-                addPullRequestSenderAsAssignee(context, repo, payload);
+                addPullSenderAsAssignee(context, repo, payload);
             }
 
             let files = null;
@@ -78,11 +82,15 @@ module.exports = (app) => {
                 const reviewComments = reviewPullRequest(repo, files);
 
                 if (reviewComments.length) {
-                    resolveComments(reviewComments);
+                    postComments(
+                        context,
+                        reviewComments,
+                        delayBetweenCommentRequestsInSeconds
+                    );
                 }
 
                 if (isReviewSummaryEnabled) {
-                    addSummaryComment(context, reviewComments, payload);
+                    postSummary(context, reviewComments, payload);
                 }
             } catch (error) {
                 app.log.error(error);
@@ -90,110 +98,3 @@ module.exports = (app) => {
         }
     );
 };
-
-function setup(context) {
-    const payload = context.payload;
-
-    const repo = context.repo();
-
-    repo.pull_number = payload.number;
-
-    return {
-        payload,
-        repo,
-    };
-}
-
-async function resolveStrictWorkflow(context, payload, strictWorkflow) {
-    const comment = rules.pull['strictWorkflow'].invoke(payload);
-
-    let isReviewAborted = false;
-
-    if (comment.reason === 'prefix') {
-        isReviewAborted = strictWorkflow.abortReviewOnInvalidBranchPrefix;
-    } else if (comment.reason === 'flow') {
-        isReviewAborted = strictWorkflow.abortReviewOnInvalidFlow;
-    }
-
-    try {
-        await addComment(context, dedent(comment.body));
-    } catch (error) {
-        probotInstance.log.error(error);
-    }
-
-    return isReviewAborted;
-}
-
-async function addPullRequestSenderAsAssignee(context, repo, payload) {
-    const { login: pullRequestOwner } = payload.sender;
-
-    try {
-        await addAssignees(context, repo, [pullRequestOwner]);
-    } catch (error) {
-        probotInstance.log.error(error);
-    }
-}
-
-function reviewPullRequest(repo, files) {
-    let reviewComments = [];
-
-    for (let i = 0; i < files.length; i++) {
-        const file = { ...files[i], ...repo };
-
-        const comments = Pepega.investigate(file).against(rules);
-
-        reviewComments = [...reviewComments, ...comments];
-    }
-
-    return reviewComments;
-}
-
-async function resolveComments(app, context, reviewComments) {
-    for (let i = 0; i < reviewComments.length; i++) {
-        const reviewComment = reviewComments[i];
-
-        if (reviewComment.start_line) {
-            try {
-                await addMultiLineReviewComment(context, {
-                    ...reviewComment,
-                });
-            } catch (error) {
-                app.log.error(error);
-            }
-        } else if (reviewComment.line) {
-            try {
-                await addSingleLineReviewComment(context, {
-                    ...reviewComment,
-                });
-            } catch (error) {
-                app.log.error(error);
-            }
-        }
-
-        await timer(settings.delayBetweenCommentRequestsInSeconds * 1000);
-    }
-}
-
-async function addSummaryComment(context, reviewComments, payload) {
-    const { commits, additions, deletions, changed_files } =
-        payload.pull_request;
-
-    const commentBody = `<em>pull request review completed</em>
-
-    :speech_balloon: ${
-        reviewComments.length
-            ? `${reviewComments.length} comment(s) require attention.`
-            : `0 comments added :star: :star:`
-    } 
-    :hammer: ${commits} commit(s)
-    :heavy_plus_sign: ${additions} additions
-    :heavy_minus_sign: ${deletions} deletions
-    :heavy_division_sign: ${changed_files} changed files
-    `;
-
-    try {
-        await addComment(context, dedent(commentBody));
-    } catch (error) {
-        probotInstance.log.error(error);
-    }
-}
