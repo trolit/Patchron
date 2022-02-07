5f8f9d0a26c737178385acd1f31e73043c19963c
pawel idzikowski <iddziku@gmail.com>
2022-02-07 20:22:49 +0100

    Create keyword-import.js

---
+++ test/keyword-import.js
@@ -0,0 +1,16 @@
+import something1 from '../pepega/pull-request/addSenderAsAssignee';
+import something2 from '@/test/folder2/mySomething155';
+import something3 from '@/test/folder1/mySomething';
+
+import something4 from '@/wpw/foaa/eeee';
+
+import something555 from 'test1/wrz/teeeeeeee';
+
+module.exports = () => {
+    // TODO: some code1
+    // TODO: some code2
+    // TODO: some code3
+    // TODO: some code4
+};
+
+import something9888 from '@/components/something/111';
