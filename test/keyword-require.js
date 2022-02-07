da17f57d82d709b5bd01eb5e45950f305f4e6f90
pawel idzikowski <iddziku@gmail.com>
2022-02-07 20:22:52 +0100

    Create keyword-require.js

---
+++ test/keyword-require.js
@@ -0,0 +1,16 @@
+const something1 = require('../pepega/pull-request/addSenderAsAssignee');
+const something2 = require('@/test/folder2/mySomething155');
+const something3 = require('@/test/folder1/mySomething');
+
+const something4 = require('@/wpw/foaa/eeee');
+
+const something555 = require('test1/wrz/teeeeeeee');
+
+module.exports = () => {
+    // TODO: some code1
+    // TODO: some code2
+    // TODO: some code3
+    // TODO: some code4
+};
+
+const something9888 = require('@/components/something/111');
