9effc20524f810e9e407b0adb517bc5d039a5d07
pawel idzikowski <iddziku@gmail.com>
2022-02-07 23:12:49 +0100

    Create KeywordsOrderedByLength.js

---
+++ pepega/rules/common/KeywordsOrderedByLength.js
@@ -0,0 +1,35 @@
+class KeywordsOrderedByLengthRule {
+    /**
+     * @param {object} config
+     * @param {Array<{name: string, regex: string, order: 'ascending'|'descending', ignoreNewline: boolean }>} config.keywords
+     * @param {string} config.keywords[].name - readable name
+     * @param {string} config.keywords[].regex - regular expression to match keyword
+     * @param {string} config.keywords[].order -
+     * @param {string} config.keywords[].ignoreNewline - the name of an employee.
+     */
+    constructor(config) {
+        const { keywords } = config;
+
+        this.keywords = keywords;
+    }
+
+    invoke(file) {
+        if (!this.keywords.length) {
+            probotInstance.log.error(
+                `Couldn't run rule ${__filename} on ${file.filename}. No keywords defined.`
+            );
+
+            return [];
+        }
+
+        const { split_patch: splitPatch } = file;
+
+        for (let rowIndex = 0; rowIndex < splitPatch.length; rowIndex++) {}
+
+        let comments = [];
+
+        return comments;
+    }
+}
+
+module.exports = KeywordsOrderedByLengthRule;
