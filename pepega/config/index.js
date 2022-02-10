9c8008a4c676779ab0599c9827e8a7b17a3ba670
pawel idzikowski <iddziku@gmail.com>
2022-02-10 15:54:36 +0100

    Update index.js

--- pepega/config/index.js
+++ pepega/config/index.js
@@ -1,6 +1,5 @@
+const js = require('./jsRules');
 const strictWorkflow = require('../rules/pull/StrictWorkflow');
-const noUnmarkedComments = require('../rules/common/NoUnmarkedComments');
-const keywordsOrderedByLength = require('../rules/common/KeywordsOrderedByLength');
 
 require('dotenv').config({
     path: '../../.env',
@@ -52,49 +51,7 @@ module.exports = {
         },
         files: {
             common: [],
-            js: [
-                {
-                    instance: new noUnmarkedComments({
-                        prefixes: [
-                            {
-                                value: 'TODO:',
-                                meaning: 'needs to be implemented',
-                            },
-                            {
-                                value: '*:',
-                                meaning: 'important note',
-                            },
-                            {
-                                value: '!:',
-                                meaning: 'to be removed',
-                            },
-                            {
-                                value: '?:',
-                                meaning: 'suggestion',
-                            },
-                            {
-                                value: 'TMP:',
-                                meaning: 'temporary solution',
-                            },
-                        ],
-                        isAppliedToSingleLineComments: true,
-                        isAppliedToMultiLineComments: true,
-                        isAppliedToInlineComments: true,
-                    }),
-                },
-                {
-                    instance: new keywordsOrderedByLength({
-                        keywords: [
-                            {
-                                name: 'import',
-                                regex: '/import.*/',
-                                order: 'ascending',
-                                ignoreNewline: true,
-                            },
-                        ],
-                    }),
-                },
-            ],
+            js,
         },
     },
 };
