5b060583055df252b339cff96e3033d2cf92f7a2
pawel idzikowski <iddziku@gmail.com>
2022-02-20 19:53:23 +0100

    Update test.vue

--- pepega/config/test.vue
+++ pepega/config/test.vue
@@ -8,9 +8,20 @@
 import test1 from 'test1/test2/test3';
 import test5 from 'test5';
 
+
+import test99 from 'test5';
+import test11 from 'test15';
+
+import test4 from 'test53';
+
+
+
+import test555 from 'test5333';
+
 export default() {
 
 }
 
 import gg from 'zz/ww/zz';
+import gg2 from 'zz/ww/zz';
 </script>
