47d78c818e87df387a19529e633532d1894a5b70
pawel idzikowski <iddziku@gmail.com>
2022-02-20 18:57:52 +0100

    Create test.vue

---
+++ pepega/config/test.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+      test
+  </div>
+<template>
+
+<script>
+import test1 from 'test1/test2/test3';
+import test5 from 'test5';
+
+export default() {
+
+}
+
+import gg from 'zz/ww/zz';
+</script>
