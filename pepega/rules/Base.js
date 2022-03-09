fec31c24d004753b5722a1fee337aae9a69292bf
pawel idzikowski <iddziku@gmail.com>
2022-03-09 20:44:32 +0100

    Update Base.js

--- pepega/rules/Base.js
+++ pepega/rules/Base.js
@@ -2,6 +2,15 @@ const getLineNumber = require('../helpers/getLineNumber');
 const ReviewCommentBuilder = require('../builders/ReviewComment');
 
 class BaseRule {
+    constructor() {
+        const merge = '<<< merge >>>';
+        const newLine = '<<< new line >>>';
+
+        this.merge = merge;
+        this.newLine = newLine;
+        this.customLines = [newLine, merge];
+    }
+
     /**
      * @returns {object}
      */
