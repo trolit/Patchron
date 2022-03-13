13f285bbd0c58cdbc81e96984141eda425df6127
pawel idzikowski <iddziku@gmail.com>
2022-03-13 22:26:09 +0100

    Move regex based initialization to Base.js file

--- pepega/rules/common/PositionedKeywords.js
+++ pepega/rules/common/PositionedKeywords.js
@@ -48,7 +48,7 @@ class PositionedKeywordsRule extends BaseRule {
             }
 
             const { matchedRows, unchangedRows } =
-                this._initializeRegexBasedData(splitPatch, keyword);
+                this.initializeRegexBasedData(splitPatch, keyword);
 
             if (matchedRows.length <= 1) {
                 continue;
@@ -560,101 +560,6 @@ class PositionedKeywordsRule extends BaseRule {
         return unchangedRows.some(
             (unchangedRow) => unchangedRow.index === row.index
         );
-    }
-
-    _initializeRegexBasedData(splitPatch, keyword) {
-        let matchedRows = [];
-        let unchangedRows = [];
-
-        for (let index = 0; index < splitPatch.length; index++) {
-            const row = splitPatch[index];
-
-            if (matchedRows.length && removeWhitespaces(row) === '+') {
-                matchedRows.push({
-                    index,
-                    content: this.newLine,
-                });
-
-                continue;
-            } else if (matchedRows.length && row.startsWith('-')) {
-                matchedRows.push({
-                    index,
-                    content: this.merge,
-                });
-
-                continue;
-            } else if (matchedRows.length && row.startsWith(' ')) {
-                unchangedRows.push({
-                    index,
-                    content: row.trim(),
-                });
-            }
-
-            const matchResult = row.match(keyword.regex);
-
-            if (!matchResult) {
-                continue;
-            }
-
-            const content = matchResult[0].trim();
-
-            if (
-                keyword.multilineOptions?.length &&
-                this._isMultiline(keyword, content)
-            ) {
-                const multilineEndIndex = this._resolveMultilineMatch(
-                    keyword,
-                    splitPatch,
-                    index
-                );
-
-                matchedRows.push({
-                    index,
-                    content: splitPatch[multilineEndIndex].trim(),
-                    length: multilineEndIndex - index,
-                });
-
-                index = multilineEndIndex;
-
-                continue;
-            }
-
-            matchedRows.push({
-                index,
-                content,
-            });
-        }
-
-        return {
-            matchedRows,
-            unchangedRows,
-        };
-    }
-
-    _isMultiline(keyword, line) {
-        const { multilineOptions } = keyword;
-
-        return !multilineOptions.some((option) => line.includes(option));
-    }
-
-    _resolveMultilineMatch(keyword, splitPatch, currentIndex) {
-        let multilineEndIndex = -1;
-
-        for (let index = currentIndex + 1; index < splitPatch.length; index++) {
-            const row = splitPatch[index];
-
-            if (
-                !row.startsWith('-') &&
-                removeWhitespaces(row) !== '+' &&
-                !this._isMultiline(keyword, row)
-            ) {
-                multilineEndIndex = index;
-
-                break;
-            }
-        }
-
-        return multilineEndIndex;
     }
 
     _getCommentBody(keyword, data) {
