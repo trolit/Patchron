f0705364ed46ab453aea25be691cf60b5b91f394
pawel idzikowski <iddziku@gmail.com>
2022-03-25 18:48:14 +0100

    Create debugRule.js

---
+++ pepega/helpers/debugRule.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-console */
+
+const PositionedKeywordsRule = require('../../pepega/rules/common/PositionedKeywords');
+
+const rules = {
+    PositionedKeywordsRule,
+};
+
+/**
+ * Import that method into core **index.js** file to easily debug particular rule with particular state.
+ * @param {string} ruleName - file name of rule
+ * @param {object} config - rule config
+ * @param {object} file - provide all elements that are required by given rules
+ */
+module.exports = (ruleName, config, file) => {
+    const Rule = rules[ruleName];
+
+    const rule = new Rule(config);
+
+    const result = rule.invoke(file);
+
+    console.log(`***************************************************`);
+    console.log(`${ruleName} rule result`);
+    console.log(`***************************************************`);
+
+    console.log(result);
+};
