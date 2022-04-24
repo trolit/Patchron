72d94bc76376d771bd2dd452a3d59364000b7d31
pawel idzikowski <iddziku@gmail.com>
2022-04-24 22:10:09 +0200

    First single "if" block tests

--- test/rules/js/SingleLineBlock.test.js
+++ test/rules/js/SingleLineBlock.test.js
@@ -83,4 +83,89 @@ describe('invoke function', () => {
 
         expect(result).toEqual([]);
     });
+
+    /**
+     * ---------------------------------------------------
+     * SINGLE IF BLOCK
+     * ---------------------------------------------------
+     */
+
+    it('returns empty array on valid single-line `if` blocks (with curly braces)', () => {
+        const result = singleLineBlockRule.invoke({
+            filename: '...',
+            split_patch: [
+                `@@ -10,13 +1,7 @@\n`,
+                `+ const x = 4;\n`,
+                `+ const y = 5;\n`,
+                `+ if (x === y) { result = x; }\n`,
+                `+ \n`,
+                `+ if (x > y) {\n`,
+                `+  result = x + y;\n`,
+                `+ }\n`,
+                `+ \n`,
+                `+ if (x <= y)\n`,
+                `+ {\n`,
+                `+  result = x - y;\n`,
+                `+ }\n`,
+                `+ \n`,
+                `+ for (let index = 0; index < 10; index++) {\n`,
+                `+  if (index % 2 === 0) {\n`,
+                `+      console.log(index);\n`,
+                `+  }\n`,
+                `+  \n`,
+                `+  if (index % 3 === 0) {\n`,
+                `+      if (result > 20) { continue; }\n`,
+                `+      else { break; }\n`,
+                `+  }\n`,
+                `+ }\n`
+            ]
+        });
+
+        expect(result).toEqual([]);
+    });
+
+    it('returns review on invalid single-line `if` blocks (with curly braces)', () => {
+        const result = singleLineBlockRule.invoke({
+            filename: '...',
+            split_patch: [
+                `@@ -10,13 +1,7 @@\n`,
+                `+ const x = 4;\n`,
+                `+ const y = 5;\n`,
+                `+ if (x === y) result = x;\n`,
+                `+ \n`,
+                `+ if (x > y)\n`,
+                `+  result = x + y;\n`,
+                `+ \n`,
+                `+ if (x <= y)\n`,
+                `+  result = x - y;\n`,
+                `+ \n`,
+                `+ for (let index = 0; index < 10; index++) {\n`,
+                `+  if (index % 2 === 0)\n`,
+                `+      console.log(index);\n`,
+                `+  \n`,
+                `+  if (index % 3 === 0) {\n`,
+                `+      if (result > 20) continue;\n`,
+                `+      else break;\n`,
+                `+  }\n`,
+                `+ }\n`
+            ]
+        });
+
+        expect(result).toHaveLength(6);
+
+        expect(result[0]).toHaveProperty('line', 3);
+
+        expect(result[1]).toHaveProperty('start_line', 5);
+        expect(result[1]).toHaveProperty('position', 6);
+
+        expect(result[2]).toHaveProperty('start_line', 8);
+        expect(result[2]).toHaveProperty('position', 9);
+
+        expect(result[3]).toHaveProperty('start_line', 12);
+        expect(result[3]).toHaveProperty('position', 13);
+
+        expect(result[4]).toHaveProperty('line', 16);
+
+        expect(result[5]).toHaveProperty('line', 17);
+    });
 });
