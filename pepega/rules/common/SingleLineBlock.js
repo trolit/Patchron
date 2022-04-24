e246bb2b717980c64f6de79868892a387459b38e
pawel idzikowski <iddziku@gmail.com>
2022-04-24 23:11:35 +0200

    Update SingleLineBlockRule documentation

--- pepega/rules/common/SingleLineBlock.js
+++ pepega/rules/common/SingleLineBlock.js
@@ -3,7 +3,9 @@ const getContentNesting = require('../../helpers/getContentNesting');
 
 class SingleLineBlockRule extends BaseRule {
     /**
-     * Please note that, if provided block does not end by curly brace or single line, assign regular expression under `endIndicator` property to specify block end manually. For instance `endIndicator` of `do..while` could be ```/^while/```
+     * Please note that:
+     * - if provided block does not end by curly brace or single line, assign regular expression under `endIndicator` property to specify block end manually. For instance `endIndicator` of `do..while` could be ```/^while/```
+     * - if blocks have similiar parts, pay attention to their order e.g. if..else if..else and/or regular expression
      * @param {object} config
      * @param {Array<{name: string, expression: object, endIndicator: object?}>} config.blocks
      * @param {boolean} config.curlyBraces - true indicates that matched blocks should be wrapped with curly braces {}
