aebf8d4b25f2fc5697ea155b4cae11afe4a9fff3
pawel idzikowski <iddziku@gmail.com>
2022-04-28 21:54:02 +0200

    Reworking SingleLineBlock <>

--- pepega/rules/common/SingleLineBlock.js
+++ pepega/rules/common/SingleLineBlock.js
@@ -4,7 +4,7 @@ const getContentNesting = require('../../helpers/getContentNesting');
 class SingleLineBlockRule extends BaseRule {
     /**
      * Please note that:
-     * - if provided block does not end by curly brace or single line, assign regular expression under `endIndicator` property to specify block end manually. For instance `endIndicator` of `do..while` could be ```/^while/```
+     * - if provided block does not end by curly brace or single line, assign regular expression under `endIndicator` property to specify block end manually. For instance `endIndicator` of `do..while` could be ```/while/``` since we have no 100% certainity that it will always appear at the start of the line.
      * - if blocks have similiar parts, pay attention to their order e.g. if..else if..else and/or regular expression
      * - each block's expression should end with `.*`
      * @param {object} config
@@ -62,8 +62,160 @@ class SingleLineBlockRule extends BaseRule {
         );
     }
 
+    _getSingleLineBlocks(data, contentNesting) {
+        const rowsToSkip = [];
+        const singleLineBlocks = [];
+        const dataLength = data.length;
+
+        for (let index = 0; index < dataLength; index++) {
+            const row = data[index];
+            const { content } = row;
+
+            if (
+                this.CUSTOM_LINES.includes(content) ||
+                rowsToSkip.includes(index)
+            ) {
+                continue;
+            }
+
+            const block = this._findMatchingBlock(content);
+
+            if (!block) {
+                continue;
+            }
+
+            const nextRow = index + 1 < data.length ? data[index + 1] : null;
+
+            const rowNesting = this._findRowNesting(
+                contentNesting,
+                row,
+                nextRow
+            );
+
+            let to = -1;
+            const from = row.index;
+
+            if (block?.endIndicator) {
+                to = this._getEndIndicatorIndex(data, block, row);
+
+                rowsToSkip.push(to);
+            } else if (rowNesting) {
+                to = rowNesting.to;
+            } else {
+                to = this._getEndIndex(block, row);
+            }
+
+            if (this._isSingleLineBlock(data, from, to, rowNesting)) {
+                singleLineBlocks.push({
+                    from,
+                    to,
+                    isWithBraces: !!rowNesting
+                });
+            }
+        }
+
+        return singleLineBlocks;
+    }
+
+    _isSingleLineBlock(data, from, to, rowNesting) {
+        return (
+            from === to ||
+            to - from - this._countMergeLines(data, from, to) === 1 ||
+            this._countBlockLength(data, from, to, rowNesting) === 1
+        );
+    }
+
+    _countMergeLines(data, from, to) {
+        const partOfData = data.slice(from, to + 1);
+
+        return partOfData.filter(({ content }) => content === this.MERGE)
+            .length;
+    }
+
+    _countBlockLength(data, from, to, rowNesting) {
+        if (rowNesting && from + 1 >= data?.length) {
+            return -1;
+        }
+
+        let startIndex = 0;
+
+        if (rowNesting) {
+            const nextRowStartsWithCurlyBrace =
+                data[from + 1].content.startsWith('{');
+
+            startIndex = nextRowStartsWithCurlyBrace ? from + 2 : from + 1;
+        } else {
+            startIndex = from + 1;
+        }
+
+        const partOfData = data.slice(startIndex, to);
+
+        return partOfData.filter(
+            ({ content }) => !this.CUSTOM_LINES.includes(content)
+        ).length;
+    }
+
+    _findMatchingBlock(content) {
+        return this.blocks.find(({ expression }) => content.match(expression));
+    }
+
+    _findRowNesting(contentNesting, row, nextRow) {
+        const { index } = row;
+
+        const currentRowNesting = contentNesting.find(
+            ({ from }) => from === index
+        );
+
+        if (!nextRow || currentRowNesting) {
+            return currentRowNesting;
+        }
+
+        return nextRow.content.startsWith('{') ? nextRow : null;
+    }
+
+    _getEndIndicatorIndex(data, block, row) {
+        const { endIndicator } = block;
+        const partOfData = data.slice(row.index);
+
+        const foundRow = partOfData.find(
+            ({ content, indentation }) =>
+                content.match(endIndicator) && indentation >= row.indentation
+        );
+
+        return foundRow ? foundRow.index : -1;
+    }
+
+    _getEndIndex(block, row) {
+        const { content } = row;
+        const { expression } = block;
+
+        const expressionAsString = expression.toString().replace(/\//g, '');
+
+        const rawBlockExpression = expressionAsString.endsWith('.*')
+            ? expressionAsString.slice(0, -2)
+            : expressionAsString;
+
+        const rawBlockMatch = content.match(rawBlockExpression);
+
+        if (!rawBlockMatch) {
+            return -1;
+        }
+
+        const rawBlock = rawBlockMatch[0];
+
+        const contentLength = content.length;
+        const rawBlockLength = rawBlock.length;
+
+        const extraBlockContent = content.substr(rawBlockLength).trim();
+
+        return contentLength > rawBlockLength &&
+            !this.isLineCommented(extraBlockContent)
+            ? row.index
+            : row.index + 1;
+    }
+
     _reviewSingleLineBlocks(file, singleLineBlocks) {
-        let reviewComments = [];
+        const reviewComments = [];
 
         for (const singleLineBlock of singleLineBlocks) {
             const { from, to, isWithBraces } = singleLineBlock;
@@ -89,205 +241,6 @@ class SingleLineBlockRule extends BaseRule {
         }
 
         return reviewComments;
-    }
-
-    _getSingleLineBlocks(data, contentNesting) {
-        const endIndicators = [];
-        const singleLineBlocks = [];
-        const dataLength = data.length;
-
-        for (let index = 0; index < dataLength; index++) {
-            const row = data[index];
-            const { content } = row;
-
-            if (endIndicators?.length) {
-                const endIndicatorIndex = endIndicators.findIndex(
-                    (endIndicator) => content.match(endIndicator)
-                );
-
-                if (~endIndicatorIndex) {
-                    endIndicators.splice(endIndicatorIndex, 1);
-
-                    continue;
-                }
-            }
-
-            if (this.CUSTOM_LINES.includes(content)) {
-                continue;
-            }
-
-            const block = this._findMatchingBlock(content);
-
-            if (!block) {
-                continue;
-            }
-
-            const nextRow = index + 1 < dataLength ? data[index + 1] : null;
-
-            const isWithBraces = this._isWithBraces(
-                contentNesting,
-                row,
-                nextRow
-            );
-
-            let to;
-            const from = row.index;
-
-            if (block?.endIndicator) {
-                to = this._getEndIndicatorIndex(data, block, row);
-
-                endIndicators.push(block.endIndicator);
-            } else {
-                to = this._getEndIndex(
-                    contentNesting,
-                    block,
-                    row,
-                    isWithBraces
-                );
-            }
-
-            if (this._isSingleLineBlock(data, from, to, isWithBraces)) {
-                singleLineBlocks.push({
-                    from,
-                    to,
-                    isWithBraces
-                });
-            }
-        }
-
-        return singleLineBlocks;
-    }
-
-    _isSingleLineBlock(data, from, to, isWithBraces) {
-        return (
-            from === to ||
-            to - from - this._countMergeLines(data, from, to) === 1 ||
-            this._countBlockLength(data, from, to, isWithBraces) === 1
-        );
-    }
-
-    _countMergeLines(data, from, to) {
-        const partOfData = data.slice(from, to + 1);
-
-        return partOfData.filter(({ content }) => content === this.MERGE)
-            .length;
-    }
-
-    _countBlockLength(data, from, to, isWithBraces) {
-        if (isWithBraces && from + 1 >= data?.length) {
-            return -1;
-        }
-
-        let startIndex = 0;
-
-        if (isWithBraces) {
-            const nextRowStartsWithCurlyBrace =
-                data[from + 1].content.startsWith('{');
-
-            startIndex = nextRowStartsWithCurlyBrace ? from + 2 : from + 1;
-        } else {
-            startIndex = from + 1;
-        }
-
-        const partOfData = data.slice(startIndex, to);
-
-        return partOfData.filter(
-            ({ content }) => !this.CUSTOM_LINES.includes(content)
-        ).length;
-    }
-
-    _findMatchingBlock(content) {
-        return this.blocks.find(({ expression }) => content.match(expression));
-    }
-
-    _isWithBraces(contentNesting, row, nextRow = null) {
-        const { index } = row;
-
-        if (!nextRow) {
-            return contentNesting.some(({ from }) => from === index);
-        }
-
-        return contentNesting.some(
-            ({ from }) => from === index || nextRow.content.startsWith('{')
-        );
-    }
-
-    _getEndIndicatorIndex(data, block, row) {
-        let endIndex = -1;
-        let matchesToSkip = 0;
-
-        const dataLength = data.length;
-        const { endIndicator } = block;
-        const firstNextRow =
-            row.index + 1 < dataLength ? data[row.index + 1] : null;
-
-        if (row.content.match(endIndicator)) {
-            return row.index;
-        }
-
-        if (firstNextRow?.content.match(endIndicator)) {
-            return row.index + 1;
-        }
-
-        for (let index = row.index + 1; index < dataLength; index++) {
-            const nextRow = data[index];
-            const nextRowBlock = this._findMatchingBlock(nextRow.content);
-
-            if (block === nextRowBlock) {
-                matchesToSkip++;
-
-                continue;
-            }
-
-            const isEndIndicator = nextRow.content.match(block.endIndicator);
-
-            if (isEndIndicator && matchesToSkip) {
-                matchesToSkip--;
-            } else if (isEndIndicator && !matchesToSkip) {
-                endIndex = index;
-
-                break;
-            }
-        }
-
-        return endIndex;
-    }
-
-    _getEndIndex(contentNesting, block, row, isWithBraces) {
-        if (isWithBraces) {
-            const { to } = contentNesting.find(({ from }) =>
-                [row.index, row.index + 1].includes(from)
-            );
-
-            return to;
-        } else {
-            const { content } = row;
-            const { expression } = block;
-
-            const expressionAsString = expression.toString().replace(/\//g, '');
-
-            const rawBlockExpression = expressionAsString.endsWith('.*')
-                ? expressionAsString.slice(0, -2)
-                : expressionAsString;
-
-            const rawBlockMatch = content.match(rawBlockExpression);
-
-            if (!rawBlockMatch) {
-                return -1;
-            }
-
-            const rawBlock = rawBlockMatch[0];
-
-            const contentLength = content.length;
-            const rawBlockLength = rawBlock.length;
-
-            const extraBlockContent = content.substr(rawBlockLength).trim();
-
-            return contentLength > rawBlockLength &&
-                !this.isLineCommented(extraBlockContent)
-                ? row.index
-                : row.index + 1;
-        }
     }
 
     /**
