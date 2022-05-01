4d8cfd559d652d0070c7fd217629447a401dc0b2
pawel idzikowski <iddziku@gmail.com>
2022-05-01 21:39:18 +0200

    Update configureLogger.js

--- pepega/utilities/configureLogger.js
+++ pepega/utilities/configureLogger.js
@@ -1,54 +1,12 @@
 const {
     settings: { isStoringLogsEnabled }
 } = require('../config');
+const setupLogBasePathJob = require('../utilities/setupLogBasePathJob');
 
 module.exports = (app) => {
-    global.log = isStoringLogsEnabled ? configureLogsStoring() : app.log;
-};
-
-function configureLogsStoring() {
-    const stream = require('stream');
-    const logThrough = new stream.PassThrough();
-
-    const childProcess = require('child_process');
-
-    const pinoms = require('pino-multi-stream');
-    const prettyStream = pinoms.prettyStream();
-
-    const streams = [{ stream: logThrough }, { stream: prettyStream }];
-    const log = pinoms(pinoms.multistream(streams));
-
-    const dir = `${__dirname}/../../.logs`;
-
-    const fs = require('fs');
-    const path = require('path');
-
-    const { env } = process;
-    const cwd = process.cwd();
-
-    if (!fs.existsSync(dir)) {
-        fs.mkdirSync(dir);
+    if (isStoringLogsEnabled) {
+        setupLogBasePathJob();
+    } else {
+        global.log = app.log;
     }
-
-    const basePath = path.join(`${__dirname}`, '..', '..', '.logs');
-
-    const child = childProcess.spawn(
-        process.execPath,
-        [
-            require.resolve('pino-tee'),
-            'warn',
-            `${basePath}/warn`,
-            'error',
-            `${basePath}/error`,
-            'fatal',
-            `${basePath}/fatal`,
-            'info',
-            `${basePath}/info`
-        ],
-        { cwd, env }
-    );
-
-    logThrough.pipe(child.stdin);
-
-    return log;
-}
+};
