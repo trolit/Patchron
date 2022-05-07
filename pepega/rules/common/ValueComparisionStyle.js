232e6496b9660a3d2f64266a0a1b7f5b6da3c4be
pawel idzikowski <iddziku@gmail.com>
2022-05-07 18:29:10 +0200

    Update ValueComparisionStyle Rule docs

--- pepega/rules/common/ValueComparisionStyle.js
+++ pepega/rules/common/ValueComparisionStyle.js
@@ -2,15 +2,22 @@ const BaseRule = require('../Base');
 
 class ValueComparisionStyleRule extends BaseRule {
     /**
-     * Allows to set expected equality/inequality comparement convention.
+     * Allows to set expected equality/inequality comparement convention. Rule by default covers basic patterns which are:
+     * - `... (condition) ...`
+     * - `condition ? ... : ...`
+     * - interpolated cases (backticks)
+     *
+     * If you would like to add Vue condition (v-if), add expression to `specificPatterns` array.
      *
      * **available level** options:
      * - 0 - weak
      * - 1 - strict
      * - 2 - Object.is (ES6 feature)
      * @param {object} config
-     * @param {number} config.level
+     * @param {Array<number>} config.allowedLevels
+     * @param {Array<object>} config.specificPatterns
      *
+     * @link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
      * @link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Equality_comparisons_and_sameness
      * @link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Strict_inequality
      */
