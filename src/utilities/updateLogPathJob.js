ee4cf44f4a3a14083b08cd7c5021066a751ee347
pawel idzikowski <iddziku@gmail.com>
2022-05-14 16:57:02 +0200

    Improve cron job readability

--- src/utilities/updateLogPathJob.js
+++ src/utilities/updateLogPathJob.js
@@ -4,36 +4,45 @@ const { CronJob } = require('cron');
 const pinoms = require('pino-multi-stream');
 
 module.exports = (eventEmitter) => {
+    const start = true;
+    const context = null;
+    const runOnInit = true;
+    const onComplete = null;
+    const cronTime = '0 0 0 * * *';
+    const timeZone = 'Europe/Warsaw';
+
+    const onTick = () => {
+        const logsParentDirectory = `${__dirname}/../../.logs`;
+
+        if (!fs.existsSync(logsParentDirectory)) {
+            fs.mkdirSync(logsParentDirectory);
+        }
+
+        const date = dayjs().format('YYYY-MM-DD');
+
+        const prettyStream = pinoms.prettyStream();
+
+        const streams = [
+            {
+                stream: fs.createWriteStream(
+                    `${logsParentDirectory}/${date}.log`
+                )
+            },
+            { stream: prettyStream }
+        ];
+
+        log = pinoms(pinoms.multistream(streams));
+
+        eventEmitter.emit('path-updated', log);
+    };
+
     new CronJob(
-        '0 0 0 * * *',
-        function () {
-            const logsParentDirectory = `${__dirname}/../../.logs`;
-
-            if (!fs.existsSync(logsParentDirectory)) {
-                fs.mkdirSync(logsParentDirectory);
-            }
-
-            const date = dayjs().format('YYYY-MM-DD');
-
-            const prettyStream = pinoms.prettyStream();
-
-            const streams = [
-                {
-                    stream: fs.createWriteStream(
-                        `${logsParentDirectory}/${date}.log`
-                    )
-                },
-                { stream: prettyStream }
-            ];
-
-            log = pinoms(pinoms.multistream(streams));
-
-            eventEmitter.emit('path-updated', log);
-        },
-        null,
-        true,
-        'Europe/Warsaw',
-        undefined,
-        true
+        cronTime,
+        onTick,
+        onComplete,
+        start,
+        timeZone,
+        context,
+        runOnInit
     );
 };
