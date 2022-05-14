431f86a3c7da95c79c54fbfd3491e5e246b00af3
pawel idzikowski <iddziku@gmail.com>
2022-05-14 17:31:53 +0200

    Update addAssignees.js

--- src/github/addAssignees.js
+++ src/github/addAssignees.js
@@ -3,16 +3,14 @@ const { logFatal } = require('../utilities/EventLog');
 /**
  * adds assignees to pull request
  * @param {WebhookEvent<EventPayloads.WebhookPayloadPullRequest>} context WebhookEvent instance.
- * @param {object} repo
- * @param {string} repo.owner repository owner's name
- * @param {object} repo.repo repository name
- * @param {object} repo.pull_number pull request Id
  * @param {Array<string>} assignees usernames of people to assign
  *
  * @link
  * https://octokit.github.io/rest.js/v18#issues-add-assignees
  */
-module.exports = async (context, repo, assignees) => {
+module.exports = async (context, assignees) => {
+    const repo = context.repo();
+
     try {
         await context.octokit.issues.addAssignees({
             ...repo,
