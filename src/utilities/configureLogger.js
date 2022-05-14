598110ac2ebed96b39cd7ac1dd1799a6dde5ff97
pawel idzikowski <iddziku@gmail.com>
2022-05-14 18:19:22 +0200

    Update configureLogger.js

--- src/utilities/configureLogger.js
+++ src/utilities/configureLogger.js
@@ -3,18 +3,19 @@ const EventEmitter = require('events');
 const {
     settings: { isStoringLogsEnabled }
 } = require('../config');
+const EventLog = require('./EventLog');
 const updateLogPathJob = require('./updateLogPathJob');
 
 module.exports = (app, pepegaContext) => {
     const eventEmitter = new EventEmitter();
 
     eventEmitter.on('path-updated', (updatedLog) => {
-        pepegaContext.log = updatedLog;
+        pepegaContext.log = new EventLog(updatedLog);
     });
 
     if (isStoringLogsEnabled) {
         updateLogPathJob(eventEmitter);
     } else {
-        pepegaContext.log = app.log;
+        pepegaContext.log = new EventLog(app.log);
     }
 };
