aa2853cbb7e9b1ef52f956c829076b0a8fd4c46e
pawel idzikowski <iddziku@gmail.com>
2022-05-18 22:25:29 +0200

    Update ReviewComment builder

--- src/builders/ReviewComment.js
+++ src/builders/ReviewComment.js
@@ -4,11 +4,13 @@ class ReviewCommentBuilder {
      *
      * @param {import('../builders/PepegaContext')} pepegaContext
      */
-    constructor(pepegaContext) {
+    constructor(pepegaContext, file) {
         const { pullRequest, repo } = pepegaContext;
+        const { commitId: commit_id } = file;
 
         this.basicInformation = {
             ...repo,
+            commit_id,
             pull_number: pullRequest.id
         };
     }
@@ -19,18 +21,16 @@ class ReviewCommentBuilder {
      * @param {string} data.body review comment
      * @param {string} data.line line number
      * @param {string} data.side which side line refers to (LEFT=deletion, RIGHT=addition)
-     * @param {string} data.commit_id
      * @returns {object}
      */
     buildSingleLineComment(data) {
-        const { body, line, side, commit_id } = data;
+        const { body, line, side } = data;
 
         const comment = {
             ...this.basicInformation,
             body,
             line,
-            side,
-            commit_id
+            side
         };
 
         return comment;
@@ -43,19 +43,17 @@ class ReviewCommentBuilder {
      * @param {string} data.start_line line number (counted from line after '@@')
      * @param {string} data.start_side which side line refers to (LEFT=deletion, RIGHT=addition)
      * @param {string} data.position number of lines to take into review (counted from line after '@@')
-     * @param {string} data.commit_id
      * @returns {object}
      */
     buildMultiLineComment(data) {
-        const { body, start_line, start_side, position, commit_id } = data;
+        const { body, start_line, start_side, position } = data;
 
         const comment = {
             ...this.basicInformation,
             body,
             start_line,
             start_side,
-            position,
-            commit_id
+            position
         };
 
         return comment;
