90b6fd47063bef4cf3e6b5863ce0c3ccc5cb1f00
pawel idzikowski <iddziku@gmail.com>
2022-05-19 18:37:49 +0200

    Update addSingleLineReviewComment.js

--- src/github/addSingleLineReviewComment.js
+++ src/github/addSingleLineReviewComment.js
@@ -1,28 +1,22 @@
 /**
  * sends request to add single line review comment.
- * @param {import('../builders/PepegaContext')} pepegaContext
- * @param {object} payload review details.
- * @param {string} payload.owner repository owner's name
- * @param {string} payload.repo repository name
- * @param {string} payload.pull_number pull request Id
- * @param {string} payload.body review comment
- * @param {string} payload.line line number (counted from the start of the file)
- * @param {string} payload.side which side line refers to (LEFT=deletion, RIGHT=addition)
- * @param {string} payload.path relative path to the file (tl;dr; - filename)
- * @param {string} payload.commit_id
+ * @param {PepegaContext} pepegaContext
+ * @param {SingleLineComment} singleLineComment
  *
  * @link
  * https://octokit.github.io/rest.js/v18#pulls-create-review-comment
  *
  * @returns {object} request response
  */
-module.exports = async (pepegaContext, payload) => {
+module.exports = async (pepegaContext, singleLineComment) => {
     const { pullRequest, log } = pepegaContext;
     const { context } = pullRequest;
     let result = null;
 
     try {
-        result = await context.octokit.pulls.createReviewComment(payload);
+        result = await context.octokit.pulls.createReviewComment(
+            singleLineComment
+        );
     } catch (error) {
         log.fatal(error);
     }
