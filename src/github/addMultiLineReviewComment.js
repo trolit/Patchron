e9924dd3c14bd497778c249253e7b0d297d82e75
pawel idzikowski <iddziku@gmail.com>
2022-05-19 18:27:41 +0200

    Update addMultiLineReviewComment.js

--- src/github/addMultiLineReviewComment.js
+++ src/github/addMultiLineReviewComment.js
@@ -1,29 +1,22 @@
 /**
  * sends request to add multi line review comment.
- * @param {import('../builders/PepegaContext')} pepegaContext
- * @param {object} payload review details.
- * @param {string} payload.owner repository owner's name
- * @param {string} payload.repo repository name
- * @param {string} payload.pull_number pull request Id
- * @param {string} payload.body review comment
- * @param {string} payload.start_line line number (counted from line after '@@')
- * @param {string} payload.start_side which side line refers to (LEFT=deletion, RIGHT=addition)
- * @param {string} payload.position number of lines to take into review (counted from line after '@@')
- * @param {string} payload.path relative path to the file (tl;dr; - filename)
- * @param {string} payload.commit_id
+ * @param {PepegaContext} pepegaContext
+ * @param {MultiLineComment} multiLineComment
  *
  * @link
  * https://octokit.github.io/rest.js/v18#pulls-create-review-comment
  *
  * @returns {object} request response
  */
-module.exports = async (pepegaContext, payload) => {
+module.exports = async (pepegaContext, multiLineComment) => {
     const { pullRequest, log } = pepegaContext;
     const { context } = pullRequest;
     let result = null;
 
     try {
-        result = await context.octokit.pulls.createReviewComment(payload);
+        result = await context.octokit.pulls.createReviewComment(
+            multiLineComment
+        );
     } catch (error) {
         log.fatal(error);
     }
