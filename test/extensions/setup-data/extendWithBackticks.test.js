d05644e1f0b841f820a99efb982ef410ade4228c
pawel idzikowski <iddziku@gmail.com>
2022-05-28 13:46:33 +0200

    Minor polishes

--- test/extensions/setup-data/extendWithBackticks.test.js
+++ test/extensions/setup-data/extendWithBackticks.test.js
@@ -1,4 +1,5 @@
 const { describe, expect, it } = require('@jest/globals');
+
 const extendWithBackticks = require('src/extensions/setup-data/extendWithBackticks');
 
 describe('invoke function', () => {
