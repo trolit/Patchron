4ca2bc09540a4f2c63c8d746d9ef18bf901e87a9
pawel idzikowski <iddziku@gmail.com>
2022-05-29 22:45:35 +0200

    Create typedef for DirectImportRule

---
+++ src/config/type-definitions/rules/js/DirectImport.js
@@ -0,0 +1,10 @@
+/**
+ * @typedef {object} Package
+ * @property {string} name readable name
+ * @property {object} expression
+ */
+
+/**
+ * @typedef {object} DirectImportConfig
+ * @property {Array<Package>} packages
+ */
