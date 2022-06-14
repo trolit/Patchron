96f64f22d1616dd7248a431b7aa9a0a62fec22a5
pawel idzikowski <iddziku@gmail.com>
2022-06-14 17:35:05 +0200

    Update vueRules.js
    Update ImportWithoutExtension.js

--- src/config/vueRules.js
+++ src/config/vueRules.js
@@ -45,6 +45,8 @@ module.exports = [
     {
         enabled: false,
         reference: ImportWithoutExtensionRule,
-        config: {}
+        config: {
+            type: 'module'
+        }
     }
 ];
--- src/rules/js/ImportWithoutExtension.js
+++ src/rules/js/ImportWithoutExtension.js
@@ -1,8 +1,19 @@
+/// <reference path="../../config/type-definitions/rules/js/ImportWithoutExtension.js" />
+
 const BaseRule = require('src/rules/Base');
 
 class ImportWithoutExtensionRule extends BaseRule {
+    /**
+     * @param {PepegaContext} pepegaContext
+     * @param {ImportWithoutExtensionConfig} config
+     * @param {Patch} file
+     */
     constructor(pepegaContext, config, file) {
         super(pepegaContext, file);
+
+        const { type } = config;
+
+        this.type = type;
     }
 
     invoke() {}
