4bffeb7952186373dccc08b5a9718761be0ae07a
pawel idzikowski <iddziku@gmail.com>
2022-06-25 20:19:15 +0200

    Init invoke method on SimplePropertyAssignmentRule

--- src/rules/js/SimplePropertyAssignment.js
+++ src/rules/js/SimplePropertyAssignment.js
@@ -11,6 +11,25 @@ class SimplePropertyAssignmentRule extends BaseRule {
     }
 
     invoke() {
+        // ((\w+):.*?)[,|}] - single line or comma line
+        // ((\w+):.*) - last, without comma
+
+        const { splitPatch } = this.file;
+
+        const data = this.setupData(splitPatch);
+        const dataLength = data.length;
+
+        for (let index = 0; index < dataLength; index++) {
+            const row = data[index];
+            const { trimmedContent } = row;
+
+            if (
+                trimmedContent.startsWith('@@') ||
+                this.CUSTOM_LINES.includes(trimmedContent)
+            ) {
+                continue;
+            }
+        }
     }
 
     /**
