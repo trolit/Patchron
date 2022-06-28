670af83d79102962475d9fd74156249f93449636
pawel idzikowski <iddziku@gmail.com>
2022-06-28 17:48:29 +0200

    Update SimplePropertyAssignmentRule comment

--- src/rules/js/SimplePropertyAssignment.js
+++ src/rules/js/SimplePropertyAssignment.js
@@ -116,7 +116,7 @@ class SimplePropertyAssignmentRule extends BaseRule {
      * @returns {string}
      */
     _getCommentBody() {
-        return `It seems that there is redundant value assignment to property (assigned value has the same name as key)`;
+        return `It seems that there is redundant value assignment (assigned value has the same name as key) :thinking:`;
     }
 }
 
