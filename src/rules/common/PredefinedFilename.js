1ca85c0e69c2b54d572f3407c118b7e0680c6a1d
pawel idzikowski <iddziku@gmail.com>
2022-06-29 18:03:58 +0200

    Add documentation for PredefinedFilenameRule

--- src/rules/common/PredefinedFilename.js
+++ src/rules/common/PredefinedFilename.js
@@ -2,8 +2,10 @@ const BaseRule = require('src/rules/Base');
 
 class PredefinedFilenameRule extends BaseRule {
     /**
+     * imposes specific filename structure. Restrictions are checked in order from shortest path to longest. Path containing asterisk or slash only is not allowed. End path with asterisk if you want to allow rule to match any number of levels after declared path e.g. `test/backend/*`.
+     *
      * @param {PatchronContext} patchronContext
-     * @param {object} config
+     * @param {PredefinedFilenameConfig} config
      * @param {Patch} file
      */
     constructor(patchronContext, config, file) {
