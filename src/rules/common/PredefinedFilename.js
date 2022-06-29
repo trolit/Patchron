5a79df3675b21fe666bd4238a20f3c26446aea3f
pawel idzikowski <iddziku@gmail.com>
2022-06-29 18:21:31 +0200

    Sort restrictions by path length and filter out * or / cases

--- src/rules/common/PredefinedFilename.js
+++ src/rules/common/PredefinedFilename.js
@@ -1,8 +1,9 @@
+const sortBy = require('lodash/sortBy');
 const BaseRule = require('src/rules/Base');
 
 class PredefinedFilenameRule extends BaseRule {
     /**
-     * imposes specific filename structure. Restrictions are checked in order from shortest path to longest. Path containing asterisk or slash only is not allowed. End path with asterisk if you want to allow rule to match any number of levels after declared path e.g. `test/backend/*`.
+     * imposes specific filename structure. Restrictions are checked in order from shortest path to longest. Path containing asterisk or slash only is ignored. End path with asterisk if you want to allow rule to match any number of levels after declared path e.g. `test/backend/*`.
      *
      * @param {PatchronContext} patchronContext
      * @param {PredefinedFilenameConfig} config
@@ -10,6 +11,14 @@ class PredefinedFilenameRule extends BaseRule {
      */
     constructor(patchronContext, config, file) {
         super(patchronContext, file);
+
+        const { restrictions } = config;
+
+        const filteredRestrictions = restrictions.filter(
+            ({ path }) => path !== '*' || path !== '/'
+        );
+
+        this.restrictions = sortBy(filteredRestrictions, 'path');
     }
 
     invoke() {
