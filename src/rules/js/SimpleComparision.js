631d38d8a29dff5e6a075bf4724b2823c82f2ddb
pawel idzikowski <iddziku@gmail.com>
2022-07-04 19:19:54 +0200

    `SimpleComparisionRule` setup

--- src/rules/js/SimpleComparision.js
+++ src/rules/js/SimpleComparision.js
@@ -2,22 +2,68 @@ const BaseRule = require('src/rules/Base');
 
 class SimpleComparisionRule extends BaseRule {
     /**
-     *
      * @param {PatchronContext} patchronContext
-     * @param {object} config
+     * @param {SimpleComparisionRuleConfig} config
      * @param {Patch} file
      */
     constructor(patchronContext, config, file) {
         super(patchronContext, file);
+
+        const { patterns } = config;
+
+        this.patterns = patterns;
     }
 
-    invoke() {}
+    invoke() {
+        if (!this.patterns?.length) {
+            this.log.warning(
+                __filename,
+                'No patterns defined. Rule review skipped.',
+                this.file
+            );
+
+            return [];
+        }
+
+        const reviewComments = [];
+        const { splitPatch } = this.file;
+        const data = this.setupData(splitPatch);
+        const dataLength = data.length;
+
+        for (let index = 0; index < dataLength; index++) {
+            const { trimmedContent } = data[index];
+
+            if (this.CUSTOM_LINES.includes(trimmedContent)) {
+                continue;
+            }
+
+            const matchedPattern = this.patterns.find((pattern) =>
+                trimmedContent.match(pattern.expression)
+            );
+
+            if (matchedPattern) {
+                reviewComments.push(
+                    this.getSingleLineComment({
+                        body: this._getCommentBody(matchedPattern),
+                        index
+                    })
+                );
+            }
+        }
+
+        return reviewComments;
+    }
 
     /**
      * @returns {string}
      */
-    _getCommentBody() {
-        return `TBA`;
+    _getCommentBody(matchedPattern) {
+        const commentBody = `It seems that code can be simplified.
+
+        ${matchedPattern.comment}
+        `;
+
+        return this.dedent(commentBody);
     }
 }
 
