346e0153309184653f85682328d58603d9daeaae
pawel idzikowski <iddziku@gmail.com>
2022-07-11 21:25:29 +0200

    Init MarkedComment test file for HTML

---
+++ test/rules/vue/MarkedComment.test.js
@@ -0,0 +1,113 @@
+const nock = require('nock');
+const {
+    describe,
+    expect,
+    it,
+    beforeEach,
+    afterEach
+} = require('@jest/globals');
+
+const {
+    html: { MarkedCommentRule }
+} = require('src/rules');
+const setupPatchronContext = require('test/setupPatchronContext');
+const initializeFile = require('test/rules/helpers/initializeFile');
+
+const config = {};
+
+describe('invoke function', () => {
+    let patchronContext = null;
+    let file = {};
+
+    beforeEach(() => {
+        patchronContext = setupPatchronContext();
+
+        file = initializeFile();
+    });
+
+    afterEach(() => {
+        nock.cleanAll();
+
+        nock.enableNetConnect();
+    });
+
+    it('returns empty array on marked comments', () => {
+        const markedCommentRule = new MarkedCommentRule(
+            patchronContext,
+            config,
+            {
+                ...file,
+                splitPatch: [
+                    `@@ -10,13 +3,7 @@`,
+                    `+<template>`,
+                    `+<--`,
+                    `+@NOTE might not work perfect due to problems with API`,
+                    `-@WARNING`,
+                    `+-->`,
+                    `+<div>`,
+                    `+    <-- @TODO`,
+                    `+        Update props once PR#859 will be merged`,
+                    `+    -->`,
+                    `+    <my-custom-component`,
+                    `+        :data="data"`,
+                    `+        :labels="labels"`,
+                    `+        :container="container"`,
+                    `+        @click="onClick"`,
+                    `+    />`,
+                    `+    <-- @TMP data preview -->`,
+                    `+    {{ data }}`,
+                    `+</div>`,
+                    `+<--`
+                ]
+            }
+        );
+
+        const result = markedCommentRule.invoke();
+
+        expect(result).toEqual([]);
+    });
+
+    it('returns review on unmarked comments', () => {
+        const markedCommentRule = new MarkedCommentRule(
+            patchronContext,
+            config,
+            {
+                ...file,
+                splitPatch: [
+                    `@@ -10,13 +3,7 @@`,
+                    `+<template>`,
+                    `+<--`,
+                    `+might not work perfect due to problems with API`,
+                    `-@WARNING`,
+                    `+-->`,
+                    `+<div>`,
+                    `+    <--`,
+                    `+        Update props once PR#859 will be merged`,
+                    `+    -->`,
+                    `+    <my-custom-component`,
+                    `+        :data="data"`,
+                    `+        :labels="labels"`,
+                    `+        :container="container"`,
+                    `+        @click="onClick"`,
+                    `+    />`,
+                    `+    <-- tmp data preview -->`,
+                    `+    {{ data }}`,
+                    `+</div>`,
+                    `+<--`
+                ]
+            }
+        );
+
+        const result = markedCommentRule.invoke();
+
+        expect(result).toHaveLength(3);
+
+        expect(result[0]).toHaveProperty('start_line', 4);
+        expect(result[0]).toHaveProperty('position', 4);
+
+        expect(result[1]).toHaveProperty('start_line', 8);
+        expect(result[1]).toHaveProperty('position', 8);
+
+        expect(result[2]).toHaveProperty('line', 17);
+    });
+});
