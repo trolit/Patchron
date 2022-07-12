913786eed4329009f99e26e992ba2173eba19fee
pawel idzikowski <iddziku@gmail.com>
2022-07-12 17:51:52 +0200

    MarkedComment (HTML) implementation

--- src/rules/html/MarkedComment.js
+++ src/rules/html/MarkedComment.js
@@ -2,6 +2,7 @@ const BaseRule = require('src/rules/Base');
 
 class MarkedCommentRule extends BaseRule {
     /**
+     * ensures that in HTML comment block at least one line has to begin with one of the predefined prefixes.
      *
      * @param {PatchronContext} patchronContext
      * @param {object} config
@@ -9,10 +10,98 @@ class MarkedCommentRule extends BaseRule {
      */
     constructor(patchronContext, config, file) {
         super(patchronContext, file);
+
+        const { prefixes } = config;
+
+        this.prefixes = prefixes;
+        this.COMMENT_END = '-->';
+        this.COMMENT_START = '<!--';
     }
 
     invoke() {
-        return [];
+        const reviewComments = [];
+
+        const { splitPatch } = this.file;
+        const splitPatchLength = splitPatch.length;
+
+        for (let index = 0; index < splitPatchLength; index++) {
+            const row = splitPatch[index];
+
+            if (row.startsWith(this.DELETED)) {
+                continue;
+            }
+
+            const rawRow = this.getRawContent(row).trim();
+
+            if (
+                rawRow.startsWith('@@') ||
+                !rawRow.includes(this.COMMENT_START)
+            ) {
+                continue;
+            }
+
+            if (rawRow.endsWith(this.COMMENT_END)) {
+                const splitRawRow = rawRow.split(this.COMMENT_START);
+                const comment =
+                    splitRawRow.length === 2 ? splitRawRow[1].trim() : null;
+
+                if (comment && !this._startsWithPrefix(comment)) {
+                    reviewComments.push(
+                        this.getSingleLineComment({
+                            body: this._getCommentBody(),
+                            index
+                        })
+                    );
+                }
+
+                continue;
+            }
+
+            const endIndex = this._resolveMultiLineComment(splitPatch, index);
+
+            if (~endIndex) {
+                reviewComments.push(
+                    this.getMultiLineComment({
+                        body: this._getCommentBody(),
+                        from: index,
+                        to: endIndex
+                    })
+                );
+            }
+        }
+        return reviewComments;
+    }
+
+    _resolveMultiLineComment(splitPatch, currentIndex) {
+        const splitPatchLength = splitPatch.length;
+
+        for (let index = currentIndex; index < splitPatchLength; index++) {
+            const row = splitPatch[index];
+
+            if (row.startsWith(this.DELETED)) {
+                continue;
+            }
+
+            const rawRow = this.getRawContent(row).trim();
+
+            const fixedRow = rawRow.startsWith(this.COMMENT_START)
+                ? rawRow.slice(4).trim()
+                : rawRow;
+
+            if (this._startsWithPrefix(fixedRow)) {
+                break;
+            }
+
+            if (fixedRow.endsWith(this.COMMENT_END)) {
+                return index;
+            }
+        }
+
+        return -1;
+    }
+
+    _startsWithPrefix(row) {
+        return this.prefixes.some(({ value }) => row.startsWith(value));
     }
 
     /**
