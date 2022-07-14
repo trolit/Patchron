1f8573121bade1a1ba5b22765df675fcd25c00c0
pawel idzikowski <iddziku@gmail.com>
2022-07-14 17:23:49 +0200

    Return to previous name of naming rules (without Rule suffix)

--- src/rules/index.js
+++ src/rules/index.js
@@ -1,5 +1,14 @@
 const requireDirectory = require('require-directory');
 
-module.exports = requireDirectory(module, {
-    include: /^.*Rule.js/
+const rules = requireDirectory(module, {
+    exclude: /.*(Base|review).js/,
+    rename: (name) => {
+        const firstCharacter = name.charAt(0);
+
+        return firstCharacter === firstCharacter.toLowerCase()
+            ? name
+            : `${name}Rule`;
+    }
 });
+
+module.exports = rules;
