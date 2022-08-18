f362cdfe98eb686c1ac64704304812d7a996a662
pawel idzikowski <iddziku@gmail.com>
2022-08-18 21:39:35 +0200

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,11 +1,8 @@
 /* eslint-disable no-inline-comments */
 
 const fs = require('fs');
+const jsonpath = require('jsonpath');
 const dotenvParseVariables = require('dotenv-parse-variables');
-
-const js = require('./jsRules');
-const vue = require('./vueRules');
-const pull = require('./pullRules');
 
 const env = '.env';
 const defaultEnv = '.env.default';
@@ -16,16 +13,41 @@ const dotenv = require('dotenv').config({
 
 const parsedEnv = dotenvParseVariables(dotenv.parsed);
 
+const rulesConfig = require('src/config/rules');
+
+for (const category in rulesConfig) {
+    const rules = rulesConfig[category];
+
+    for (const rule of rules) {
+        const { rulename, config } = rule;
+
+        rule.reference = require(`src/rules/${rulename}`);
+
+        delete rule.rulename;
+
+        jsonpath.apply(config, '$..regex', (value) => {
+            return new RegExp(value);
+        });
+    }
+}
+
 const {
     SENDERS,
     IS_STORING_LOGS_ENABLED,
     MAX_COMMENTS_PER_REVIEW,
+    RULES_CONFIGURATION_URL,
     IS_REVIEW_SUMMARY_ENABLED,
     IS_OWNER_ASSIGNING_ENABLED,
     IS_GET_FILES_REQUEST_PAGINATED,
     APPROVE_PULL_ON_EMPTY_REVIEW_COMMENTS,
     DELAY_BETWEEN_COMMENT_REQUESTS_IN_SECONDS
 } = parsedEnv;
+
+if (RULES_CONFIGURATION_URL) {
+    // attempt to fetch config from URL
+} else {
+    // attempt to fetch config from app
+}
 
 module.exports = {
     settings: {
@@ -40,11 +62,5 @@ module.exports = {
         approvePullOnEmptyReviewComments: APPROVE_PULL_ON_EMPTY_REVIEW_COMMENTS
     },
     nodeEnvironment: process.env.NODE_ENV,
-    rules: {
-        pull,
-        files: {
-            js,
-            vue
-        }
-    }
+    rules: rulesConfig
 };
