14c406194d814dc2f09d438c51e994bdf5f9a86b
pawel idzikowski <iddziku@gmail.com>
2022-08-29 22:14:39 +0200

    Update configureRules.js

--- src/utilities/configureRules.js
+++ src/utilities/configureRules.js
@@ -1,17 +1,36 @@
 const jsonpath = require('jsonpath');
 const isPlainObject = require('lodash/isPlainObject');
 
+/**
+ * takes collection of rules configuration (json file) and adjusts it by:
+ * - swapping `rulename` for rule instance through `reference` property
+ * - parsing all config properties that are named `regex`
+ *
+ * @param {object} rules
+ */
 module.exports = (rules) => {
     for (const categoryKey in rules) {
         const category = rules[categoryKey];
 
         if (isPlainObject(category)) {
             for (const subCategoryKey in category) {
+                const subCategory = category[subCategoryKey];
+
+                if (isPlainObject(subCategory)) {
+                    for (const subSubCategoryKey in subCategory) {
+                        _setupRules(subCategory[subSubCategoryKey]);
+                    }
+
+                    continue;
+                }
+
                 _setupRules(category[subCategoryKey]);
             }
-        } else {
-            _setupRules(category);
+
+            continue;
         }
+
+        _setupRules(category);
     }
 
     return rules;
