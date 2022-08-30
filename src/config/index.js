098b3b2acf63a7527b909d4624dab0f74c3b940b
pawel idzikowski <iddziku@gmail.com>
2022-08-30 18:37:48 +0200

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -35,51 +35,51 @@ const {
     DELAY_BETWEEN_COMMENT_REQUESTS_IN_SECONDS
 } = env;
 
-const rules = require(RULES_CONFIGURATION_PATH ||
-    DEFAULT_RULES_CONFIGURATION_PATH);
+const rules = require(RULES_CONFIGURATION_PATH);
 
 const configuredRules = configureRules(rules);
 
 module.exports = {
     nodeEnvironment,
     rules: configuredRules,
-    rulesConfigurationUrl:
-        RULES_CONFIGURATION_URL || DEFAULT_RULES_CONFIGURATION_URL,
-    rulesConfigurationPath:
-        RULES_CONFIGURATION_PATH || DEFAULT_RULES_CONFIGURATION_PATH,
+    rulesConfigurationUrl: RULES_CONFIGURATION_URL,
+    rulesConfigurationPath: RULES_CONFIGURATION_PATH,
     settings: {
-        senders: SENDERS || DEFAULT_SENDERS,
-        isStoringLogsEnabled:
-            IS_STORING_LOGS_ENABLED || DEFAULT_IS_STORING_LOGS_ENABLED,
-        maxCommentsPerReview:
-            MAX_COMMENTS_PER_REVIEW || DEFAULT_MAX_COMMENTS_PER_REVIEW,
+        senders: SENDERS,
+        isStoringLogsEnabled: IS_STORING_LOGS_ENABLED,
+        maxCommentsPerReview: MAX_COMMENTS_PER_REVIEW,
         delayBetweenCommentRequestsInSeconds:
-            DELAY_BETWEEN_COMMENT_REQUESTS_IN_SECONDS ||
-            DEFAULT_DELAY_BETWEEN_COMMENT_REQUESTS_IN_SECONDS,
-        isReviewSummaryEnabled:
-            IS_REVIEW_SUMMARY_ENABLED || DEFAULT_IS_REVIEW_SUMMARY_ENABLED,
-        isOwnerAssigningEnabled:
-            IS_OWNER_ASSIGNING_ENABLED || DEFAULT_IS_OWNER_ASSIGNING_ENABLED,
-        isGetFilesRequestPaginated:
-            IS_GET_FILES_REQUEST_PAGINATED ||
-            DEFAULT_IS_GET_FILES_REQUEST_PAGINATED,
-        approvePullOnEmptyReviewComments:
-            APPROVE_PULL_ON_EMPTY_REVIEW_COMMENTS ||
-            DEFAULT_APPROVE_PULL_ON_EMPTY_REVIEW_COMMENTS
+            DELAY_BETWEEN_COMMENT_REQUESTS_IN_SECONDS,
+        isReviewSummaryEnabled: IS_REVIEW_SUMMARY_ENABLED,
+        isOwnerAssigningEnabled: IS_OWNER_ASSIGNING_ENABLED,
+        isGetFilesRequestPaginated: IS_GET_FILES_REQUEST_PAGINATED,
+        approvePullOnEmptyReviewComments: APPROVE_PULL_ON_EMPTY_REVIEW_COMMENTS
     }
 };
 
 function _setupEnv() {
-    let parsed = null;
+    let parsed = {
+        SENDERS: DEFAULT_SENDERS,
+        APPROVE_PULL_ON_EMPTY_REVIEW_COMMENTS:
+            DEFAULT_APPROVE_PULL_ON_EMPTY_REVIEW_COMMENTS,
+        DELAY_BETWEEN_COMMENT_REQUESTS_IN_SECONDS:
+            DEFAULT_DELAY_BETWEEN_COMMENT_REQUESTS_IN_SECONDS,
+        IS_STORING_LOGS_ENABLED: DEFAULT_IS_STORING_LOGS_ENABLED,
+        RULES_CONFIGURATION_URL: DEFAULT_RULES_CONFIGURATION_URL,
+        MAX_COMMENTS_PER_REVIEWS: DEFAULT_MAX_COMMENTS_PER_REVIEW,
+        RULES_CONFIGURATION_PATH: DEFAULT_RULES_CONFIGURATION_PATH,
+        IS_REVIEW_SUMMARY_ENABLED: DEFAULT_IS_REVIEW_SUMMARY_ENABLED,
+        IS_OWNER_ASSIGNING_ENABLED: DEFAULT_IS_OWNER_ASSIGNING_ENABLED,
+        IS_GET_FILES_REQUEST_PAGINATED: DEFAULT_IS_GET_FILES_REQUEST_PAGINATED
+    };
 
     if (nodeEnvironment === GITHUB_ACTION_ENVIRONMENT) {
-        parsed = {
+        const githubEnv = {
             SENDERS: process.env.SENDERS,
             IS_GET_FILES_REQUEST_PAGINATED:
                 process.env.IS_GET_FILES_REQUEST_PAGINATED,
             APPROVE_PULL_ON_EMPTY_REVIEW_COMMENTS:
                 process.env.APPROVE_PULL_ON_EMPTY_REVIEW_COMMENTS,
-            IS_STORING_LOGS_ENABLED: DEFAULT_IS_STORING_LOGS_ENABLED,
             DELAY_BETWEEN_COMMENT_REQUESTS_IN_SECONDS:
                 process.env.DELAY_BETWEEN_COMMENT_REQUESTS_IN_SECONDS,
             RULES_CONFIGURATION_URL: process.env.RULES_CONFIGURATION_URL,
@@ -87,6 +87,17 @@ function _setupEnv() {
             RULES_CONFIGURATION_PATH: process.env.RULES_CONFIGURATION_PATH,
             IS_REVIEW_SUMMARY_ENABLED: process.env.IS_REVIEW_SUMMARY_ENABLED,
             IS_OWNER_ASSIGNING_ENABLED: process.env.IS_OWNER_ASSIGNING_ENABLED
+        };
+
+        Object.keys(githubEnv).forEach((key) => {
+            if (githubEnv[key] === undefined) {
+                delete githubEnv[key];
+            }
+        });
+
+        parsed = {
+            ...parsed,
+            ...githubEnv
         };
     } else {
         const dotenv = require('dotenv').config({
